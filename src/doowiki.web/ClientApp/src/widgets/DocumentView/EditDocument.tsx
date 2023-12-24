5c1c09db816c490c7594ad61ff36548352d2613c
Mike Barton <mike__barton@hotmail.com>
2023-12-24 11:29:40 +1000

    update edit form to use primitive radix form

--- src/doowiki.web/ClientApp/src/widgets/DocumentView/EditDocument.tsx
+++ src/doowiki.web/ClientApp/src/widgets/DocumentView/EditDocument.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import useWikiApi, { SaveDocumentCommand} from '../../api/useWikiApi';
+import useWikiApi, { SaveDocumentCommand } from '../../api/useWikiApi';
 import { DocumentDto } from '../../api/api.generated.clients';
-import { TextField, TextArea } from '@radix-ui/themes';
 import { Flex, Span, Em, Button } from '../../components';
 import { ISpaceContext, SpaceContext } from '../../utils/GlobalContextProvider';
-import { useParams, useSearchParams, useNavigate } from 'react-router-dom';
+import { useSearchParams, useNavigate } from 'react-router-dom';
 import useSecurity from '../../utils/useSecurity';
+import * as Form from '@radix-ui/react-form';
 
 interface IEditDocumentProps {
     DocumentId: string | undefined,
@@ -14,68 +14,87 @@ interface IEditDocumentProps {
 const EditDocument = ({ DocumentId }: IEditDocumentProps) => {
     const wikiApi = useWikiApi();
     const security = useSecurity();
-    const [document, setDocument] = React.useState<DocumentDto>();
-    const {SpaceId, SetSpaceId} = React.useContext<ISpaceContext>(SpaceContext);
+    const [document, setDocument] = React.useState<DocumentDto>({ name: '', content: ''} as DocumentDto);
+    const { SpaceId, SetSpaceId } = React.useContext<ISpaceContext>(SpaceContext);
     const [searchParams, setSearchParams] = useSearchParams();
     const navigate = useNavigate();
-    
+
 
     React.useEffect(() => {
         async function getDocument() {
             const doc = await wikiApi.GetDocument(DocumentId!);
             setDocument(doc);
         }
-        function initDocument(){
+        function initDocument() {
             const doc = {
-                spaceId: SpaceId                             
+                spaceId: SpaceId,
+                name: '',
+                content: ''
             } as DocumentDto;
             setDocument(doc);
         }
-        if(DocumentId)
+        if (DocumentId)
             getDocument();
-        else 
+        else
             initDocument();
     }, [DocumentId]);
 
-    function onCancel(){
-        if(DocumentId)
+    function onCancel() {
+        if (DocumentId)
             navigate('/home/' + DocumentId);
         else
             navigate('/home');
     }
 
-    function onSave(){        
-        async function doSave(){
+    function onSave() {
+        async function doSave() {
             const args = {
                 name: document?.name,
                 content: document?.content,
                 documentId: document?.documentId,
                 spaceId: document?.spaceId ?? SpaceId,
-                parentId: searchParams.get('parentId')           
+                parentId: searchParams.get('parentId')
             } as SaveDocumentCommand
             await wikiApi.SaveDocument(args);
             navigate('/home/' + document?.documentId);
         }
 
-        if(security.CanWrite())
+        if (security.CanWrite())
             doSave();
     }
 
     return (
         <>
             <Flex direction={'column'} justify={'flex-start'} align={'stretch'} width={'100%'} height={'100%'} padding={[2]} >
-                <TextField.Input value={document?.name} onChange={e => setDocument({ ...document, name: e.target.value } as DocumentDto)} mb={'3'}/>
-                <Flex align={'center'} margin={[0,0,5,0]}>
-                    <Em>Created By:</Em>    
-                    <Span margin={[0,0,0,1]}>{document?.authorName}</Span>
-                </Flex>
-                <Flex direction='row' align='stretch' grow={1}>
-                    <TextArea value={document?.content} style={{ flexGrow: 1}} onChange={e => setDocument({ ...document, content: e.target.value } as DocumentDto)} mb={'3'} />
-                </Flex>
-                <Flex gap={3}>
-                    <Button onClick={onCancel}>Cancel</Button>
-                    <Button onClick={onSave}>Save</Button>
-                </Flex>
+                <Form.Root onSubmit={onSave}>
+                    <Form.Field name='title'>
+                        <Flex margin={[0, 0, 2, 0]}>
+                            <Form.Control asChild>
+                                <input type='text' value={document?.name} onChange={e => setDocument({ ...document, name: e.target.value } as DocumentDto)} />
+                            </Form.Control>
+                        </Flex>
+                    </Form.Field>
+                    <Flex align={'center'} margin={[0, 0, 5, 0]}>
+                        <Em>Created By:</Em>
+                        <Span margin={[0, 0, 0, 1]}>{document?.authorName}</Span>
+                    </Flex>
+                    <Form.Field name='content'>
+                        <Flex direction='row' align='stretch' grow={1}>
+                            <Form.Control asChild>
+                                <textarea value={document?.content} style={{ flexGrow: 1 }} onChange={e => setDocument({ ...document, content: e.target.value } as DocumentDto)} />
+                            </Form.Control>
+                        </Flex>
+                    </Form.Field>
+                    <Form.Submit asChild>
+                        <Flex gap={3}>
+                            <Button>Cancel</Button>
+                            <Button>Save</Button>
+                        </Flex>
+                    </Form.Submit>
+                </Form.Root>
+
+
+
             </Flex>
         </>
     )
